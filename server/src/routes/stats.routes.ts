b4905436aab5999363718066e73c895cbbb30cfd
rahul <rg4005450@gmail.com>
2024-08-11 22:09:47 +0530

    stats

--- server/src/routes/stats.routes.ts
+++ server/src/routes/stats.routes.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express'
-import { getHistory } from '../controllers/stat.controller'
+import { getAverageStats, getHistory } from '../controllers/stat.controller'
 import { verifyJWT } from '../middleware/auth.middleware'
 
 const router = Router()
 
 router.route("/get-history").get(verifyJWT,getHistory)
+router.route("/get-average-stats").get(verifyJWT,getAverageStats)
 
 export default  router
