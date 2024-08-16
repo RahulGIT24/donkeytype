bfa0d00ef4e7739d758f222c09203ad6a46e123f
steel77-7 <utkarshkaundal1970@gmail.com>
2024-08-16 18:06:19 +0530

    type component

--- client/src/components/TypingComponent.tsx
+++ client/src/components/TypingComponent.tsx
@@ -1,23 +1,26 @@
 import axios from "axios";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 export default function TypingComponent() {
   const [typeString, setTypeString] = useState<JSX.Element[]>([]);
-
+  const setting= useSelector((state:any)=> state.setting)
   const [focus,setFocus] = useState(false)
-  const res =
-    "In the heart of a bustling city, a small café stood nestled between towering buildings, offering a quiet retreat from the chaos outside. The aroma of freshly brewed coffee mingled with the sweet scent of pastries, drawing in weary travelers and locals alike. Soft jazz played in the background, creating a cozy atmosphere where conversations flowed freely. The walls were adorned with colorful paintings, each telling its own story. As the sun set, casting a warm glow through the large windows, the café became a haven for those seeking solace, comfort, and a brief escape from the world.";
-  const str = res.split(" ");
+  const [words,setWord]= useState('');
+  const str = words.split(" ");
 
   async function getwords(){ 
-    const response  = axios(import.meta.env.VITE_SERVER_API+`type/get-words?words=${}`,{
+    try {
+      const response  = await axios(import.meta.env.VITE_SERVER_API+`/type/get-words?words=${setting.wordNumber}`)
+      console.log(response)
       
-    })
+    } catch (error) {
+        console.log(error)
+    }
   }
 
   function formatWord(word: any) {
     let w = word.split("");
-
     return w;
   }
   function printWords() {
@@ -27,14 +30,18 @@ export default function TypingComponent() {
   }
   useEffect(() => {
     printWords();
+    
   }, []);
 
-  console.log("type string ", typeString);
+  useEffect(()=> {
+    getwords()
+  },[setting])
+ 
 
   return (
     <>
       <div
-        className={`flex h-96 max-w-[70%] flex-wrap overflow-auto text-4xl   ${!focus?' md:blur-lg':''} group-focus:`}
+        className={`flex h-96 max-w-[70%] flex-wrap overflow-auto text-4xl `}
         id="words"
       >
         
@@ -42,13 +49,13 @@ export default function TypingComponent() {
           return (
             <div className="word mx-2" key={index}>
               {element.map((e: any) => {
-                return <span className="letter">{e}</span>;
+                return <span className="letter" >{e}</span>;
               })}
             </div>
           );
         })}
       </div>
-        <div className={`focus-error text-5xl relative bottom-72  focus:group  `} > Click here to focus</div>
+        {/* <div className={`focus-error text-5xl relative bottom-72  focus:group  `} > Click here to focus</div> */}
     </>
   );
 }
